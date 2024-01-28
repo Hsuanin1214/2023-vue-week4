8e3d18fd45dfe3eab69ae839f44dfd9173d0651c
Hsuan <welcome09210801@gmail.com>
2024-01-28 22:49:00 +0800

    feat(option api) - 建立分頁元件

--- option api/products.js
+++ option api/products.js
@@ -120,8 +120,34 @@ createApp({
       backdrop:'static'
     })
   },
-}).component('pagination',{
-  template:`<p>{{pages}}</p>`,
+}).component('pagination',{ //分頁元件
+  template:`<nav aria-label="Page navigation example">
+  <ul class="pagination">
+      <li class="page-item" 
+      :class="{disabled:pages.current_page === 1}"
+      >
+          <a class="page-link" href="#" aria-label="Previous"
+          @click.prevent = "changePage(pages.current_page - 1)">
+              <span aria-hidden="true">&laquo;</span>
+          </a>
+      </li>
+      <li class="page-item" 
+      v-for="(item,index) in pages.total_pages"
+      :key="index"
+      :class="{'active': item === pages.current_page}">
+          <a class="page-link" href="#"
+          @click.prevent = "changePage(item)">
+              {{item}}
+          </a>
+      </li>
+      <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next"
+          @click.prevent = "changePage(pages.current_page + 1)">
+              <span aria-hidden="true">&raquo;</span>
+          </a>
+      </li>
+  </ul>
+</nav>`,
   props:['pages'],
   data(){
     return{
@@ -129,6 +155,8 @@ createApp({
     };
   },
   methods:{
-
+    changePage(num){
+      this.$emit('change-page',num)
+    }
   },
 }).mount('#app');
