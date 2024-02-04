e14c5e4dd9e010544e93409dcc3f348a3c58995b
Hsuan <welcome09210801@gmail.com>
2024-02-04 15:38:28 +0800

    fix(option api) : (補遺漏上傳檔案)調整vue import寫法、調整照片寫法 1. importmap 改為直接cdn方式 2. 刪除換頁多餘方法 3. 資料因第一個都會出現錯誤訊息，且openModal內渲染不出來，因此新增editProduct 避免傳參考特性 4.調整照片寫法

--- option api/components/productModalComponent.js
+++ option api/components/productModalComponent.js
@@ -1,12 +1,26 @@
 //會有refs bootstrap 問題]
 export default {
-		props: ["tempProduct", "updateProduct", "isNew"],
-		data() {
-			return {
-				productModal: null,
-			};
-		},
-		template: `<div id="productModal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
+  props: ["tempProduct", "updateProduct", "isNew"],
+  data() {
+    return {
+      productModal: null,
+      editProduct: {
+        imagesUrl: [],
+      },
+    };
+  },
+  watch: {
+    tempProduct: {
+      handler(newVal) {
+        if (newVal && !newVal.imagesUrl) {
+          newVal.imagesUrl = []; // 確保imagesUrl是一個陣列
+        }
+        this.editProduct = newVal;
+      },
+      immediate: true, // 如果希望在監視開始時立即執行一次handler，則設置immediate為true
+    },
+  },
+  template: `<div id="productModal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
 		aria-hidden="true">
 		<div class="modal-dialog modal-xl">
 				<div class="modal-content border-0">
@@ -21,35 +35,34 @@ export default {
 								<div class="row">
 										<div class="col-sm-4">
 												<div class="mb-2">
-														<div class="mb-3">
-																<label for="imageUrl" class="form-label">輸入圖片網址</label>
-																<input type="text" class="form-control mb-2" placeholder="請輸入圖片連結" id="imageUrl"
-																		v-model="tempProduct.imageUrl">
+												<div class="mb-3">
+																<input type="text" class="form-control mb-2" placeholder="請輸入圖片連結"
+																		v-model="editProduct.imageUrl">
 														</div>
-														<img class="img-fluid" :src="tempProduct.imageUrl" alt="">
+														<img class="img-fluid" :src="editProduct.imageUrl" alt="">
 												</div>
 												<h3 class="mb-3">多圖新增</h3>
 												<!-- 檢查是否為陣列 -->
-												<div v-if="Array.isArray(tempProduct.imagesUrl)">
-														<div class="mb-1" v-for="(img,key) in tempProduct.imagesUrl" :key="key + 123">
+												<div v-if="Array.isArray(editProduct.imagesUrl)">
+														<div class="mb-1" v-for="(img,key) in editProduct.imagesUrl" :key="key + 123">
 																<div class="mb-3">
-																		<input v-model="tempProduct.imagesUrl[key]" type="text" class="form-control"
+																		<input v-model="editProduct.imagesUrl[key]" type="text" class="form-control"
 																				placeholder="請輸入圖片連結">
 																</div>
 																<img class="img-fluid" :src="img" alt="">
 														</div>
 												</div>
 												<button
-														v-if="tempProduct.imagesUrl.length === 0 || tempProduct.imagesUrl[tempProduct.imagesUrl.length - 1]"
+														v-if="editProduct.imagesUrl.length == 0  || editProduct.imagesUrl[editProduct.imagesUrl.length - 1]"
 														class="btn btn-outline-primary btn-sm d-block w-100"
-														@click="tempProduct.imagesUrl.push('')">
+														@click="editProduct.imagesUrl.push('')">
 														<!-- 最後一個有值的情況下 -->
 														新增
 												</button>
 												<!-- <div v-else> -->
 												<div v-else>
 														<button class="btn btn-outline-danger btn-sm d-block w-100"
-																@click="tempProduct.imagesUrl.pop()">
+																@click="editProduct.imagesUrl.pop()">
 																刪除
 														</button>
 												</div>
@@ -58,19 +71,19 @@ export default {
 												<div class="mb-3">
 														<label for="title" class="form-label">標題</label>
 														<input id="title" type="text" class="form-control" placeholder="請輸入標題"
-																v-model="tempProduct.title">
+																v-model="editProduct.title">
 												</div>
 
 												<div class="row">
 														<div class="mb-3 col-md-6">
 																<label for="category" class="form-label">分類</label>
 																<input id="category" type="text" class="form-control" placeholder="請輸入分類"
-																		v-model="tempProduct.category">
+																		v-model="editProduct.category">
 														</div>
 														<div class="mb-3 col-md-6">
 																<label for="price" class="form-label">單位</label>
 																<input id="unit" type="text" class="form-control" placeholder="請輸入單位"
-																		v-model="tempProduct.unit">
+																		v-model="editProduct.unit">
 														</div>
 												</div>
 
@@ -78,12 +91,12 @@ export default {
 														<div class="mb-3 col-md-6">
 																<label for="origin_price" class="form-label">原價</label>
 																<input id="origin_price" type="number" min="0" class="form-control"
-																		placeholder="請輸入原價" v-model="tempProduct.origin_price">
+																		placeholder="請輸入原價" v-model="editProduct.origin_price">
 														</div>
 														<div class="mb-3 col-md-6">
 																<label for="price" class="form-label">售價</label>
 																<input id="price" type="number" min="0" class="form-control" placeholder="請輸入售價"
-																		v-model="tempProduct.price">
+																		v-model="editProduct.price">
 														</div>
 												</div>
 												<hr>
@@ -91,18 +104,18 @@ export default {
 												<div class="mb-3">
 														<label for="description" class="form-label">產品描述</label>
 														<textarea id="description" type="text" class="form-control" placeholder="請輸入產品描述"
-																v-model="tempProduct.description">
+																v-model="editProduct.description">
 							</textarea>
 												</div>
 												<div class="mb-3">
 														<label for="content" class="form-label">說明內容</label>
 														<textarea id="description" type="text" class="form-control" placeholder="請輸入說明內容"
-																v-model="tempProduct.content">
+																v-model="editProduct.content">
 							</textarea>
 												</div>
 												<div class="mb-3">
 														<div class="form-check">
-																<input id="is_enabled" v-model="tempProduct.is_enabled" class="form-check-input"
+																<input id="is_enabled" v-model="editProduct.is_enabled" class="form-check-input"
 																		type="checkbox" :true-value="1" :false-value="0">
 																<label class="form-check-label" for="is_enabled">是否啟用</label>
 														</div>
@@ -114,7 +127,7 @@ export default {
 								<button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
 										取消
 								</button>
-								<button type="button" class="btn btn-primary" @click="updateProduct">
+								<button type="button" class="btn btn-primary" @click.prevent="updateProduct">
 										確認
 								</button>
 						</div>
